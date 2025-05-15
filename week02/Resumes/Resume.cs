8154bd2019e194679f9967a50a622da7eacb6d44
ing-armandoramos <69744215+ing-armandoramos@users.noreply.github.com>
2025-05-14 18:05:01 -0700

    fixed spacing

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -3,8 +3,6 @@ using System;
 public class Resume
 {
     public string _name;
-
-    // Make sure to initialize your list to a new List before you use it.
     public List<Job> _jobs = new List<Job>();
 
     public void Display()
@@ -12,10 +10,9 @@ public class Resume
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
 
-        // Notice the use of the custom data type "Job" in this loop
         foreach (Job job in _jobs)
         {
-            // This calls the Display method on each job
+            
             job.Display();
         }
     }
