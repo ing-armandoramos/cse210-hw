87928771c8b6b2958f26ae68b70feca0e69e8d65
ing-armandoramos <69744215+ing-armandoramos@users.noreply.github.com>
2025-06-16 09:11:20 -0700

    shapeshomework

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Green", 5);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("White", 5, 8);
+        shapes.Add(s2);
+        Circle s3 = new Circle("Red", 7);
+        shapes.Add(s3);
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
